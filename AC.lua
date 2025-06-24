fd10eb350cf7fde3e59cfdfb44a089dcd15c426a
XLunar <149539550+XLunar@users.noreply.github.com>
2025-06-24 15:37:45 -0400

    Update AC.lua

--- AC.lua
+++ AC.lua
@@ -1,6 +1,14 @@
-if getgenv and getgenv().FZ_AutoClutchUnload then
-    getgenv().FZ_AutoClutchUnload()
-    getgenv().FZ_AutoClutchUnload = nil
+-- Prevent multiple instances
+if getgenv then
+    if getgenv().FZ_AutoClutchUnload then
+        getgenv().FZ_AutoClutchUnload()
+        getgenv().FZ_AutoClutchUnload = nil
+    end
+    if getgenv().FZ_AutoClutchActive then
+        -- Already running, don't run again
+        return function() end
+    end
+    getgenv().FZ_AutoClutchActive = true
 end
 
 print("active22")
@@ -8,18 +16,21 @@ print("active22")
 local UIS = game:GetService("UserInputService")
 local VirtualInputManager = game:GetService("VirtualInputManager")
 local holdingE = false
+local tappedThisRelease = false
 local inputBeganConn, inputEndedConn
 
 inputBeganConn = UIS.InputBegan:Connect(function(input, processed)
     if processed then return end
     if input.KeyCode == Enum.KeyCode.E and not holdingE then
         holdingE = true
+        tappedThisRelease = false
     end
 end)
 
 inputEndedConn = UIS.InputEnded:Connect(function(input, processed)
-    if input.KeyCode == Enum.KeyCode.E and holdingE then
+    if input.KeyCode == Enum.KeyCode.E and holdingE and not tappedThisRelease then
         holdingE = false
+        tappedThisRelease = true
         print("[AutoClutch] E released, tapping E 2 times!")
         for i = 1, 2 do
             VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.E, false, game)
@@ -35,6 +46,11 @@ local function unload()
     if inputBeganConn then pcall(function() inputBeganConn:Disconnect() end) inputBeganConn = nil end
     if inputEndedConn then pcall(function() inputEndedConn:Disconnect() end) inputEndedConn = nil end
     holdingE = false
+    tappedThisRelease = false
+    if getgenv then
+        getgenv().FZ_AutoClutchActive = nil
+        getgenv().FZ_AutoClutchUnload = nil
+    end
 end
 
 if getgenv then
